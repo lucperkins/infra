79e9d1edbcf5ad2bb713db1d77dacfbbfe79a478
Luc Perkins <luc@blockfi.com>
2020-07-24 12:48:57 -0700

    Experiment with KV in function

--- hello.js
+++ hello.js
@@ -2,6 +2,8 @@ addEventListener("fetch", (event) => {
   event.respondWith(hello(event.request))
 });
 
+const text = `Secret: ${name.secret}`
+
 async function hello(request) {
   return new Response("Hello world", {
     status: 200
