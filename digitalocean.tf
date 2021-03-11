fb4b9ff7e70c3e0513049998ada5ec4d2160faae
Luc Perkins <luc.perkins@datadoghq.com>
2021-03-11 12:06:21 -0800

    Add k8s cluster

--- digitalocean.tf
+++ digitalocean.tf
@@ -1,6 +1,6 @@
 resource "digitalocean_kubernetes_cluster" "dev" {
-  name   = "datadog-dev"
-  region = "sfo3"
+  name    = "datadog-dev"
+  region  = "sfo3"
   version = "1.20.2-do.0"
 
   node_pool {
@@ -8,4 +8,12 @@ resource "digitalocean_kubernetes_cluster" "dev" {
     size       = "s-2vcpu-2gb"
     node_count = 3
   }
+}
+
+resource "kubernetes" "dev" {
+  host = digitalocean_kubernetes_cluster.dev.ipv4_address
+
+  client_certificate     = digitalocean_kubernetes_cluster.dev.kube_config.0.client_certificate
+  client_key             = digitalocean_kubernetes_cluster.dev.kube_config.0.client_key
+  cluster_ca_certificate = digitalocean_kubernetes_cluster.dev.kube_config.0.cluster_ca_certificate
 }
--- providers.tf
+++ providers.tf
@@ -3,8 +3,12 @@ provider "digitalocean" {
 
   token = var.do_token
 
-  spaces_access_id = var.do_spaces_access_id
+  spaces_access_id  = var.do_spaces_access_id
   spaces_secret_key = var.do_spaces_secret_key
+}
+
+provider "kubernetes" {
+  version = "2.0.2"
 }
 
 provider "cloudflare" {
